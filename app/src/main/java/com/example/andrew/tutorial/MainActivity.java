dedae3577d5d0d3bffb9abd95b5323a6e708e1d3
Andrew <andrewcunnin@umass.edu>
2017-11-05 04:54:50 -0500

    Minor aesthetic changes

--- app/src/main/java/com/example/andrew/tutorial/MainActivity.java
+++ app/src/main/java/com/example/andrew/tutorial/MainActivity.java
@@ -74,18 +74,18 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
             Report rep = new Report(12);
             String date = "";
             switch(activeTrip.getMonth()){
-                case 0 : date = "January "; break;
-                case 1 : date = "February "; break;
-                case 2: date = "March "; break;
-                case 3: date = "April "; break;
-                case 4: date = "May "; break;
-                case 5: date = "June "; break;
-                case 6: date = "July "; break;
-                case 7: date = "August "; break;
-                case 8: date = "September "; break;
-                case 9: date = "October "; break;
-                case 10: date = "November "; break;
-                case 11: date = "December "; break;
+                case 0 : date = "January"; break;
+                case 1 : date = "February"; break;
+                case 2: date = "March"; break;
+                case 3: date = "April"; break;
+                case 4: date = "May"; break;
+                case 5: date = "June"; break;
+                case 6: date = "July"; break;
+                case 7: date = "August"; break;
+                case 8: date = "September"; break;
+                case 9: date = "October"; break;
+                case 10: date = "November"; break;
+                case 11: date = "December"; break;
             }
             rep.setDate(date);
             overallReport.addReport((activeTrip.getMonth()), rep);
